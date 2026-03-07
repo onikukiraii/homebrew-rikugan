c8334a961bdb7705ce132f7594057d617faa44e2
Masato Takayama <100172771+onikukiraii@users.noreply.github.com>
2026-03-07 21:15:42 +0900

    feat: update formula to v0.3.0

--- Formula/rikugan.rb
+++ Formula/rikugan.rb
@@ -1,28 +1,28 @@
 class Rikugan < Formula
   desc "TUI diff reviewer for AI prompts"
   homepage "https://github.com/onikukiraii/rikugan"
-  version "0.2.0"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.2.0/rikugan_0.2.0_darwin_amd64.tar.gz"
-      sha256 "97f27763b4e61a9997fbf9ef278ec1825d96143b8178cfcb4a4cd88cfeb310c4"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.3.0/rikugan_0.3.0_darwin_amd64.tar.gz"
+      sha256 "172865b8ea2a7eaecb458c96a8daef56d41c5339e1aeb8b7ea88f7a9fa6bb4a9"
     end
     on_arm do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.2.0/rikugan_0.2.0_darwin_arm64.tar.gz"
-      sha256 "46b81d71fa22d489c7345db598c1e5a909c7832d6978de3d431be25f1cdbbdb2"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.3.0/rikugan_0.3.0_darwin_arm64.tar.gz"
+      sha256 "10acabd8882fb9bf6c886c23020f8c8b6ae2de9b76533c8ef87d700ffcb46605"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.2.0/rikugan_0.2.0_linux_amd64.tar.gz"
-      sha256 "9a1d5369e27d2692f6afc2185911dadc4b6be14c182d0e3c30751454852ba689"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.3.0/rikugan_0.3.0_linux_amd64.tar.gz"
+      sha256 "527a7227fdbedce67403131a0fd7dbf67f5d92628b203192e10b1a31be683e6f"
     end
     on_arm do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.2.0/rikugan_0.2.0_linux_arm64.tar.gz"
-      sha256 "8c340b136895003d4bc206b2a5c948b491606a4d2a85f1320b168d469f65bf9e"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.3.0/rikugan_0.3.0_linux_arm64.tar.gz"
+      sha256 "e69aad40a3ad98bb0b6a961bf7c85b6e83aa02c985be3c068f17d5f6b14cc914"
     end
   end
 
