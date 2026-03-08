3b8cabea9aa9b3e9e74360925295449b04008ae8
onikukiraii <100172771+onikukiraii@users.noreply.github.com>
2026-03-08 18:15:34 +0900

    fix: bump to v0.3.1

--- Formula/rikugan.rb
+++ Formula/rikugan.rb
@@ -1,28 +1,28 @@
 class Rikugan < Formula
   desc "TUI diff reviewer for AI prompts"
   homepage "https://github.com/onikukiraii/rikugan"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.3.0/rikugan_0.3.0_darwin_amd64.tar.gz"
-      sha256 "172865b8ea2a7eaecb458c96a8daef56d41c5339e1aeb8b7ea88f7a9fa6bb4a9"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.3.1/rikugan_0.3.1_darwin_amd64.tar.gz"
+      sha256 "edc0759f46a5bbf458438a03b4127df62fdbaf38e4eef618c11339ff724fb0da"
     end
     on_arm do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.3.0/rikugan_0.3.0_darwin_arm64.tar.gz"
-      sha256 "10acabd8882fb9bf6c886c23020f8c8b6ae2de9b76533c8ef87d700ffcb46605"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.3.1/rikugan_0.3.1_darwin_arm64.tar.gz"
+      sha256 "bb53f37e29fd735af43a6e8a9bc74c4056528eda108c50cbb2ac815809ce333b"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.3.0/rikugan_0.3.0_linux_amd64.tar.gz"
-      sha256 "527a7227fdbedce67403131a0fd7dbf67f5d92628b203192e10b1a31be683e6f"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.3.1/rikugan_0.3.1_linux_amd64.tar.gz"
+      sha256 "4a6e3da0a3eb67561e48b73ad44e83a84dbbf56f94c37128ac7b2d434e78342f"
     end
     on_arm do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.3.0/rikugan_0.3.0_linux_arm64.tar.gz"
-      sha256 "e69aad40a3ad98bb0b6a961bf7c85b6e83aa02c985be3c068f17d5f6b14cc914"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.3.1/rikugan_0.3.1_linux_arm64.tar.gz"
+      sha256 "2b9d3088efb9e33b714190c0dcdce42432cdd9b384d13915a436741fd87f6833"
     end
   end
 
