0b4303d2b1cb156f6fdd659f504e9e2cb58a4536
onikukiraii <100172771+onikukiraii@users.noreply.github.com>
2026-03-15 12:32:14 +0900

    feat: update rikugan to v0.5.0

--- Formula/rikugan.rb
+++ Formula/rikugan.rb
@@ -1,28 +1,28 @@
 class Rikugan < Formula
   desc "TUI diff reviewer for AI prompts"
   homepage "https://github.com/onikukiraii/rikugan"
-  version "0.4.0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.4.0/rikugan_0.4.0_darwin_amd64.tar.gz"
-      sha256 "0fe465344a7d719594e207e7d309bc55a25f03704e5de3d80177200aaafc1d96"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.5.0/rikugan_0.5.0_darwin_amd64.tar.gz"
+      sha256 "9c284436e0810113dbe01b43feb4b49b3648d96262574b48a8a36beae960e1d2"
     end
     on_arm do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.4.0/rikugan_0.4.0_darwin_arm64.tar.gz"
-      sha256 "a55699dae6245835ebb4220b5fc1762d63497679b9adfacd38378a9780e56dd4"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.5.0/rikugan_0.5.0_darwin_arm64.tar.gz"
+      sha256 "3fdfb8690eca82f78f24137910faac9781b67705bd6946dbb786e885f0388b58"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.4.0/rikugan_0.4.0_linux_amd64.tar.gz"
-      sha256 "0ee11a753c2c14c8e328369759989052fa55ff742881e3cb8a87006ee9e1661d"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.5.0/rikugan_0.5.0_linux_amd64.tar.gz"
+      sha256 "6f681a6ea7c4972b6773709696c6c9e45e4f70dedb2353d6d8597607c7032bc0"
     end
     on_arm do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.4.0/rikugan_0.4.0_linux_arm64.tar.gz"
-      sha256 "602366d1617da0e264f8b85ec850b1405f5d41aae9f70fca9e90de835c662549"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.5.0/rikugan_0.5.0_linux_arm64.tar.gz"
+      sha256 "f79eaacf63d32a6069735aff5b1af2904f010ce0db9b8c486513a10cd49d9174"
     end
   end
 
