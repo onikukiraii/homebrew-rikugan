541dc3d23e82a17ffc6eb6c229ccbcd21ef308f0
Masato Takayama <masato.takayama@mntsq.com>
2026-05-21 11:58:48 +0900

    feat: bump rikugan to v0.6.0

--- Formula/rikugan.rb
+++ Formula/rikugan.rb
@@ -1,28 +1,28 @@
 class Rikugan < Formula
   desc "TUI diff reviewer for AI prompts"
   homepage "https://github.com/onikukiraii/rikugan"
-  version "0.5.0"
+  version "0.6.0"
   license "MIT"
 
   on_macos do
     on_intel do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.5.0/rikugan_0.5.0_darwin_amd64.tar.gz"
-      sha256 "9c284436e0810113dbe01b43feb4b49b3648d96262574b48a8a36beae960e1d2"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.6.0/rikugan_0.6.0_darwin_amd64.tar.gz"
+      sha256 "fc97f6af5e612e2f2039037663aa8951eb98e1e08209aa0dd37141972fc53d1f"
     end
     on_arm do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.5.0/rikugan_0.5.0_darwin_arm64.tar.gz"
-      sha256 "3fdfb8690eca82f78f24137910faac9781b67705bd6946dbb786e885f0388b58"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.6.0/rikugan_0.6.0_darwin_arm64.tar.gz"
+      sha256 "90239b1f7fb26aea5b5ff1b58494e09761136f17b38b7e9d68039f9a94580fa2"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.5.0/rikugan_0.5.0_linux_amd64.tar.gz"
-      sha256 "6f681a6ea7c4972b6773709696c6c9e45e4f70dedb2353d6d8597607c7032bc0"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.6.0/rikugan_0.6.0_linux_amd64.tar.gz"
+      sha256 "3fe6fdb503d81ed2b9e6e2ed7bb1fad1bf2f4aa0fe55b896ab3f5ee1c3e8c940"
     end
     on_arm do
-      url "https://github.com/onikukiraii/rikugan/releases/download/v0.5.0/rikugan_0.5.0_linux_arm64.tar.gz"
-      sha256 "f79eaacf63d32a6069735aff5b1af2904f010ce0db9b8c486513a10cd49d9174"
+      url "https://github.com/onikukiraii/rikugan/releases/download/v0.6.0/rikugan_0.6.0_linux_arm64.tar.gz"
+      sha256 "9872fe6d510fc3a55cc6a587df17af51e1e9b0619a9862a631e0c2ff6d0d4436"
     end
   end
 
